7ed6d4cdfb005cd799c96dcb4a5c7e16fc873c3c
PrajaktaDeore <prajaktadeore1993@gmail.com>
2026-03-26 15:00:50 +0530

    postegress issue resolved

--- stock_project/settings.py
+++ stock_project/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'chatsense_db'),
         'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'password'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'chatsense@123'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
